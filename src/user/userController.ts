7748c30e5c9a66e16ccb49f5e512b0c3fbb04b32
sandy <sandeshjadhav2002@gmail.com>
2024-04-14 13:34:04 +0530

    user model created

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,25 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { name, email, password } = req.body;
+    //validation
+    if (!name || !email || !password) {
+        const error = createHttpError(400,"All fileds are require");
+        return next(error)
+    }
+       
+    //Database call
+    const user = await userModel.findOne({email})
+    if(user){
+        const error = createHttpError(400,"User Already exits with this email")
+        return next(error)
+    }
+    
+
+    //proccess
+    //response
     res.json({ message: "User registered agian" });
 };
 

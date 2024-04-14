14464584f0400da89ecbcc0b826e59be45c26835
sandy <sandeshjadhav2002@gmail.com>
2024-04-14 13:58:12 +0530

    sign jwt token

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,30 +1,44 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import userModel from "./userModel";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const { name, email, password } = req.body;
     //validation
     if (!name || !email || !password) {
-        const error = createHttpError(400,"All fileds are require");
-        return next(error)
+        const error = createHttpError(400, "All fileds are require");
+        return next(error);
     }
-       
+
     //Database call
-    const user = await userModel.findOne({email})
-    if(user){
-        const error = createHttpError(400,"User Already exits with this email")
-        return next(error)
+    const user = await userModel.findOne({ email });
+    if (user) {
+        const error = createHttpError(
+            400,
+            "User Already exits with this email"
+        );
+        return next(error);
     }
 
     //password-hash
-    const hashedPassword = await bcrypt.hash(password,10)
-    
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    //proccess
+    const newUser = await userModel.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    //Token genration
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+        expiresIn: "7d",
+    });
+
     //response
-    res.json({ message: "User registered agian" });
+    res.json({ accessToken: token });
 };
 
 export { createUser };

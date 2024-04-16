dbc6000195a670e170ef04b885457aa6f4182fde
sandy <sandeshjadhav2002@gmail.com>
2024-04-16 21:41:37 +0530

    delete book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -171,4 +171,39 @@ const getSingleBook = async (
     }
 };
 
-export { createBook, updateBook, listBooks, getSingleBook };
+const deleteBook = async (req: Request, res: Response, next: NextFunction) => {
+    const bookId = req.params.bookId;
+
+    const book = await bookModel.findOne({ _id: bookId });
+
+    if (!book) {
+        return next(createHttpError(404, "Book not found"));
+    }
+
+    //Check Access
+    const _req = req as AuthRequest;
+    if (book.author.toString() !== _req.userId) {
+        return next(createHttpError(403, "You cannot delete books"));
+    }
+
+    const coverFilesSplits = book.coverImage.split("/");
+    const coverImagePublicId =
+        coverFilesSplits.at(-2) +
+        "/" +
+        coverFilesSplits.at(-1)?.split(".").at(-2);
+
+    const bookFileSplits = book.file.split("/");
+    const bookFilePublicId =
+        bookFileSplits.at(-2) + "/" + bookFileSplits.at(-1);
+
+    await cloudinary.uploader.destroy(coverImagePublicId);
+    await cloudinary.uploader.destroy(bookFilePublicId, {
+        resource_type: "raw",
+    });
+
+    await bookModel.deleteOne({ _id: bookId });
+
+    res.sendStatus(204);
+};
+
+export { createBook, updateBook, listBooks, getSingleBook, deleteBook };
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import express from "express";
-import { createBook, getSingleBook, listBooks, updateBook } from "./bookController";
+import { createBook, deleteBook, getSingleBook, listBooks, updateBook } from "./bookController";
 import multer from "multer";
 import authenticate from "../middlewares/authenticate";
 
@@ -34,5 +34,8 @@ bookRouter.patch(
 bookRouter.get('/',listBooks);
 
 bookRouter.get('/:bookId',getSingleBook)
+
+
+bookRouter.delete('/:bookId',authenticate,deleteBook)
 
 export default bookRouter;
